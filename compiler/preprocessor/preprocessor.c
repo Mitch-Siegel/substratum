4ad8a38c8411223431a240f9cf716a861cc22f0a
Mitch Siegel <mitch.siegel.a@gmail.com>
2023-06-29 12:18:09 -0400

    start work on include system for preprocessor

--- compiler/preprocessor/preprocessor.c
+++ compiler/preprocessor/preprocessor.c
@@ -1,3 +1,7 @@
+#include <libgen.h> // dirname/basename
+#include <string.h>
+#include <unistd.h> // chdir
+
 #include "util.h"
 
 /*
@@ -7,9 +11,19 @@
  *
  */
 #define ROLLING_BUFFER_SIZE 512
-char rollingData[ROLLING_BUFFER_SIZE * 2];
-int rollingDataStartPos = 0;
-int rollingBufferSize = 0;
+
+struct RollingBuffer
+{
+    char data[ROLLING_BUFFER_SIZE * 2];
+    int startPos;
+    int size;
+};
+
+void RollingBuffer_Setup(struct RollingBuffer *b)
+{
+    b->startPos = 0;
+    b->size = 0;
+}
 
 #define N_PREPROCESSOR_TOKENS 1
 int longestToken = 0;
@@ -17,53 +31,54 @@ char *preprocessorTokens[N_PREPROCESSOR_TOKENS] = {"#include "};
 int preprocessorTokenLengths[N_PREPROCESSOR_TOKENS];
 
 // add a character to the rolling buffer
-void RollingBuffer_Add(char c)
+void RollingBuffer_Add(struct RollingBuffer *b, char c)
 {
-    if (rollingBufferSize == ROLLING_BUFFER_SIZE)
+    if (b->size == ROLLING_BUFFER_SIZE)
     {
         ErrorAndExit(ERROR_INTERNAL, "Attempt to add to full rolling buffer!\n");
     }
-    rollingData[rollingDataStartPos + rollingBufferSize++] = c;
-    if ((rollingDataStartPos + rollingBufferSize) >= (ROLLING_BUFFER_SIZE * 2) || (rollingBufferSize == ROLLING_BUFFER_SIZE))
+    b->data[b->startPos + b->size++] = c;
+    if ((b->startPos + b->size) >= (ROLLING_BUFFER_SIZE * 2) || (b->size == ROLLING_BUFFER_SIZE))
     {
-        memcpy(rollingData, rollingData + ROLLING_BUFFER_SIZE, ROLLING_BUFFER_SIZE);
-        rollingBufferSize = ROLLING_BUFFER_SIZE;
-        rollingDataStartPos = 0;
+        memcpy(b->data, b->data + ROLLING_BUFFER_SIZE, ROLLING_BUFFER_SIZE);
+        b->size = ROLLING_BUFFER_SIZE;
+        b->startPos = 0;
     }
 }
 
-char RollingBuffer_Consume()
+char RollingBuffer_Consume(struct RollingBuffer *b)
 {
-    if (rollingBufferSize < 1)
+    if (b->size < 1)
     {
         ErrorAndExit(ERROR_INTERNAL, "Attempt to consume from empty rolling buffer!\n");
     }
-    char consumed = rollingData[rollingDataStartPos++];
-    rollingBufferSize--;
+    char consumed = b->data[b->startPos++];
+    b->size--;
     return consumed;
 }
 
-char *RollingBuffer_RawData()
+char *RollingBuffer_RawData(struct RollingBuffer *b)
 {
-    int dataStartP = rollingDataStartPos - ROLLING_BUFFER_SIZE;
+    int dataStartP = b->startPos - ROLLING_BUFFER_SIZE;
     if (dataStartP < 0)
     {
         dataStartP = 0;
     }
-    return &(rollingData[rollingDataStartPos + dataStartP]);
+    return &(b->data[b->startPos + dataStartP]);
 }
 
-int RollingBuffer_Size()
+int RollingBuffer_Size(struct RollingBuffer *b)
 {
-    return rollingBufferSize;
+    return b->size;
 }
 
 // return index of preprocessor token if one found, else -1
-int detectPreprocessorToken()
+// if a token is found, consume it from the buffer
+int detectPreprocessorToken(struct RollingBuffer *b)
 {
     // ensure we only scan tokens as long as the buffer's data
     // (really only relevant when we have no more data to put into the buffer)
-    int maxTokenLength = RollingBuffer_Size();
+    int maxTokenLength = RollingBuffer_Size(b);
     if (maxTokenLength >= longestToken)
     {
         maxTokenLength = longestToken;
@@ -75,12 +90,93 @@ int detectPreprocessorToken()
         {
             continue;
         }
-        if (!strncmp(preprocessorTokens[i], RollingBuffer_RawData(), preprocessorTokenLengths[i]))
+        if (!strncmp(preprocessorTokens[i], RollingBuffer_RawData(b), preprocessorTokenLengths[i]))
         {
+            for (int j = 0; j < preprocessorTokenLengths[i]; j++)
+            {
+                RollingBuffer_Consume(b);
+            }
             return i;
         }
     }
     return -1;
+}
+
+void populateBuffer(struct RollingBuffer *b, FILE *inFile)
+{
+    while ((!feof(inFile)) && (RollingBuffer_Size(b) < longestToken))
+    {
+        RollingBuffer_Add(b, fgetc(inFile));
+    }
+}
+
+void preprocessFile(FILE *inFile, FILE *outFile)
+{
+    // set up the main buffer for text input from the infile
+    struct RollingBuffer mainBuffer;
+    RollingBuffer_Setup(&mainBuffer);
+
+    while (!feof(inFile) || (RollingBuffer_Size(&mainBuffer) > 0))
+    {
+        populateBuffer(&mainBuffer, inFile);
+
+        int whichToken = detectPreprocessorToken(&mainBuffer);
+
+        if (whichToken == -1)
+        {
+            putc(RollingBuffer_Consume(&mainBuffer), stdout);
+            continue;
+        }
+
+        switch (whichToken)
+        {
+        case 0:
+        {
+            struct RollingBuffer includeStrBuf;
+            RollingBuffer_Setup(&includeStrBuf);
+
+            populateBuffer(&mainBuffer, inFile);
+
+            char firstCharOfPath = RollingBuffer_Consume(&mainBuffer);
+            if (firstCharOfPath != '"')
+            {
+                ErrorAndExit(ERROR_CODE, "Unexpected first character of #include directive: %c\n", firstCharOfPath);
+            }
+
+            char secondQuoteFound = 0;
+            while (!secondQuoteFound)
+            {
+                populateBuffer(&mainBuffer, inFile);
+                char nextCharOfPath = RollingBuffer_Consume(&mainBuffer);
+                if (nextCharOfPath == '"')
+                {
+                    secondQuoteFound = 1;
+                    break;
+                }
+                RollingBuffer_Add(&includeStrBuf, nextCharOfPath);
+            }
+
+            printf("%s\n", RollingBuffer_RawData(&includeStrBuf));
+
+            char *rawFileName = RollingBuffer_RawData(&includeStrBuf);
+            int rawFileNameLen = RollingBuffer_Size(&includeStrBuf);
+            char *includedFileName = malloc(rawFileNameLen + 1);
+            memcpy(includedFileName, rawFileName, rawFileNameLen);
+            includedFileName[rawFileNameLen] = '\0';
+            FILE *includedFile = fopen(includedFileName, "rb");
+            if (includedFile == NULL)
+            {
+                ErrorAndExit(ERROR_INVOCATION, "Unable to open included file %s\n", includedFileName);
+            }
+            free(includedFileName);
+            preprocessFile(includedFile, outFile);
+        }
+        break;
+
+        default:
+            ErrorAndExit(ERROR_INTERNAL, "Invalid preprocessor token index %d\n", whichToken);
+        }
+    }
 }
 
 int main(int argc, char **argv)
@@ -104,11 +200,25 @@ int main(int argc, char **argv)
         ErrorAndExit(ERROR_INVOCATION, "Input and output files must be different!\n");
     }
 
-    FILE *inFile = fopen(argv[1], "rb");
+    char *inFileDir = dirname(strdup(argv[1]));
+    if(chdir(inFileDir))
+    {
+        ErrorAndExit(ERROR_INTERNAL, "Unable to switch to directory %s\n", inFileDir);
+    }
+
+    char *justFileName = argv[1] + strlen(inFileDir);
+    if(justFileName[0] == '/')
+    {
+        justFileName++;
+    }
+
+    // open base input and output files
+    FILE *inFile = fopen(justFileName, "rb");
     if (inFile == NULL)
     {
-        ErrorAndExit(ERROR_INVOCATION, "Unable to open input file %s\n", argv[1]);
+        ErrorAndExit(ERROR_INVOCATION, "Unable to open input file %s\n", justFileName);
     }
+    free(inFileDir);
 
     FILE *outFile = fopen(argv[2], "wb");
     if (outFile == NULL)
@@ -116,31 +226,7 @@ int main(int argc, char **argv)
         ErrorAndExit(ERROR_INVOCATION, "Unable to open output file %s\n", argv[2]);
     }
 
-    int isEof;
-    while ((!(isEof = feof(inFile))) || (RollingBuffer_Size() > 0))
-    {
-        if (!isEof)
-        {
-            RollingBuffer_Add(fgetc(inFile));
-        }
-
-        if (isEof || RollingBuffer_Size() >= longestToken)
-        {
-            int whichToken = detectPreprocessorToken();
-            if (whichToken != -1)
-            {
-                printf("\nyeah yeah\n");
-                for (int i = 0; i < preprocessorTokenLengths[whichToken]; i++)
-                {
-                    RollingBuffer_Consume();
-                }
-            }
-            else
-            {
-                putc(RollingBuffer_Consume(), stdout);
-            }
-        }
-    }
+        preprocessFile(inFile, outFile);
 
     putc('\n', stdout);
 }
