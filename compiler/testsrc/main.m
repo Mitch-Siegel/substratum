0a3f9d344b01b2c770e6d3faced4f45ae58fc30a
Mitch Siegel <mitch.siegel.a@gmail.com>
2023-06-30 22:36:42 -0400

    remove extraneous files, add fibonnaci example

--- compiler/testsrc/main.m
+++ compiler/testsrc/main.m
@@ -32,9 +32,9 @@ fun printNum(uint32 num, uint32 newLine->uint8)
 	}
 
 	uint32 i = 0;
-	while (i <= digits)
+	while (i < digits)
 	{
-		print(outStr[digits - i]);
+		print(outStr[digits - i - 1]);
 		i = i + 1;
 	}
 
@@ -46,20 +46,18 @@ fun printNum(uint32 num, uint32 newLine->uint8)
 	return digits;
 }
 
-
-
 fun test(->void)
 {
 	uint8 i = 1;
-	while(i < 10)
+	while(i < 15)
 	{
 		uint8 j = 1;
-		while(j < 10)
+		while(j < 15)
 		{
 			uint8 printedDigits = printNum(mul(i, j), 0);
-			while(printedDigits < 6)
+			while(printedDigits < 5)
 			{
-				print(' ');
+				print(32);
 				printedDigits = printedDigits + 1;
 			}
 			j = j + 1;
@@ -68,12 +66,26 @@ fun test(->void)
 		i = i + 1;
 	}
 	print(10);
-	printNum(nMultiplications, 1);
+}
+
+fun fib(uint8 n->uint32)
+{
+	if(n > 1)
+	{
+		return fib(n - 1) + fib(n - 2);
+	}
+	return n;
 }
 
 fun main(->void)
 {
 	test();
+	uint8 i = 0;
+	while(i < 10)
+	{
+		printNum(fib(i), 1);
+		i = i + 1;
+	}
 }
 
 asm
