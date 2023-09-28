18464421646e1ab2d1353177dc346dff0eb66988
Mitch Siegel <mitch.siegel.a@gmail.com>
2023-09-27 23:51:00 -0400

    build upon basic ncurses ui idea

--- emu/emu.cpp
+++ emu/emu.cpp
@@ -53,33 +53,70 @@ void cleanupncurses()
     }
 }
 
-int tickRate = 0;
+uint32_t tickRate = 0; // how fast we tick the system
+bool keymod;           // modifier key (sticky) so that we can use normal letters/numbers to control things
+bool flatout;          // run the emulator flat-out, tick as fast as possible
+// when we tick our time will be slightly off, track this and make up for it
+int64_t leftoverMicros = 0;
 WINDOW *infoWin = nullptr;
 WINDOW *consoleWin = nullptr;
+
+void printStatus()
+{
+    mvwprintw(infoWin, 0, 0, "[%3s] IP: %08x",
+              (keymod ? "MOD" : "   "),
+              hardware.GetCore(0).ConfigRegisters()[ip]);
+
+    wprintw(infoWin, "tickrate: ");
+    if (flatout)
+    {
+        wprintw(infoWin, "FLATOUT");
+    }
+    else
+    {
+        wprintw(infoWin, "%-7d", tickRate);
+    }
+
+    wrefresh(infoWin);
+}
 
 void *HardwareThread(void *params)
 {
     std::chrono::steady_clock::time_point intervalStart = std::chrono::steady_clock::now();
-    int64_t leftoverMicros = 0;
+    std::chrono::steady_clock::time_point lastStatusPrint = intervalStart;
     while (hardware.Running())
     {
-        if (tickRate)
+        if (tickRate || flatout)
         {
             hardware.Tick();
-            mvwprintw(infoWin, 0, 0, "IP: %08x (%d instructions/sec) %ld", hardware.GetCore(0).ConfigRegisters()[ip], tickRate, leftoverMicros);
-            std::chrono::steady_clock::time_point intervalEnd = std::chrono::steady_clock::now();
-            int64_t intervalDuration = std::chrono::duration_cast<std::chrono::microseconds>(intervalEnd - intervalStart).count();
-            leftoverMicros += ((1000000.0f / tickRate) - intervalDuration);
-            if (leftoverMicros > 100)
+            if (!flatout)
             {
-                usleep(leftoverMicros);
-                leftoverMicros = 0;
+                std::chrono::steady_clock::time_point intervalEnd = std::chrono::steady_clock::now();
+                int64_t intervalDuration = std::chrono::duration_cast<std::chrono::microseconds>(intervalEnd - intervalStart).count();
+                double tickDuration = (1000000.0f / tickRate);
+                leftoverMicros += (tickDuration - intervalDuration);
+                if (leftoverMicros > 100)
+                {
+                    usleep(leftoverMicros);
+                    leftoverMicros = 0;
+                }
+                else if (leftoverMicros < (-1000 * tickDuration))
+                {
+                    leftoverMicros = 0;
+                    tickRate /= 2;
+                }
+
+                if (1000000 < (intervalEnd - lastStatusPrint).count())
+                {
+                    lastStatusPrint = intervalEnd;
+                    printStatus();
+                }
+                intervalStart = std::chrono::steady_clock::now();
             }
-            intervalStart = std::chrono::steady_clock::now();
         }
         else
         {
-            mvwprintw(infoWin, 0, 0, "IP: %08x (%d instructions/sec) %ld", hardware.GetCore(0).ConfigRegisters()[ip], tickRate, leftoverMicros);
+            printStatus();
             usleep(100000);
         }
     }
@@ -90,9 +127,9 @@ void *HardwareThread(void *params)
 int main(int argc, char *argv[])
 {
     initscr();
-    nodelay(stdscr, true);  // give us keypresses as soon as possible
+    nodelay(stdscr, true); // give us keypresses as soon as possible
     // keypad(stdscr, TRUE);   // interpret special keys (arrow keys and such)
-    noecho();               // don't automatically print back what we type
+    noecho(); // don't automatically print back what we type
     atexit(cleanupncurses);
 
     consoleWin = newwin(LINES - 1, COLS, 0, 0);
@@ -126,6 +163,21 @@ int main(int argc, char *argv[])
             switch (ch)
             {
             case KEY_UP:
+                if (flatout)
+                {
+                    flatout = false;
+                    printStatus();
+                    break;
+                }
+
+                if (keymod)
+                {
+                    flatout = true;
+                    keymod = false;
+                    printStatus();
+                    break;
+                }
+
                 if (!tickRate)
                 {
                     tickRate = 1;
@@ -134,9 +186,17 @@ int main(int argc, char *argv[])
                 {
                     tickRate *= 2;
                 }
+                printStatus();
                 break;
 
             case KEY_DOWN:
+                if (flatout)
+                {
+                    flatout = false;
+                    printStatus();
+                    break;
+                }
+
                 if (tickRate)
                 {
                     if (tickRate == 1)
@@ -148,19 +208,53 @@ int main(int argc, char *argv[])
                         tickRate /= 2;
                     }
                 }
+                printStatus();
                 break;
 
+            case KEY_RIGHT:
+            case KEY_LEFT:
+                keymod = !keymod;
+                printStatus();
+                break;
+
+            case '0':
+                tickRate = 0;
+                keymod = false;
+                printStatus();
+                break;
+
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (keymod)
+                {
+                    tickRate = 1;
+                    uint8_t intermediate = ch - '0';
+                    while (intermediate--)
+                    {
+                        tickRate *= 3;
+                    }
+                    keymod = false;
+                    printStatus();
+                    break;
+                }
+
             default:
-                // mvwprintw(infoWin, 0, 25, "%c:%d", ch, ch);
+                mvwprintw(infoWin, 0, 45, "%c:%d", ch, ch);
                 break;
             }
         }
         else
         {
+            wrefresh(infoWin);
             usleep((1000000.0 / 60.0));
         }
-        wrefresh(stdscr);
-        wrefresh(infoWin);
     }
 
     pthread_join(hwThread, nullptr);
