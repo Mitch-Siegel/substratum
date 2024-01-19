c9ee12a6cbd1ce1367c4447f02a35e20f5bf59e9
Mitch <mitch.siegel.a@gmail.com>
2024-01-18 22:37:37 -0500

    fix build scripts, move common test functions to separate header and object

--- tests/buildtest.sh
+++ tests/buildtest.sh
@@ -10,16 +10,22 @@ fi
 
 cd .. && make && cd -
 
+cd common
+ls *.sb | cut -d '.' -f1 | xargs -I {} sh -c "../../sbcc -i {}.sb -o {}.S -I ../common && riscv64-unknown-elf-as -r -o {}.o {}.S || exit 255"
+cd -
+
 cd $1
 
 echo ""
 echo "compiling files..."
 # xargs -I {} sh -c "echo {}; ls -la {}"
 rm -f build/*
-ls *.ca | cut -d '.' -f1 | xargs -I {} sh -c "../../sbcc -i {}.ca -o {}.S -I include && riscv64-unknown-elf-as -r -o {}.o {}.S || exit 255"
+ls *.sb | cut -d '.' -f1 | xargs -I {} sh -c "../../sbcc -i {}.sb -o {}.S -I ../common && riscv64-unknown-elf-as -r -o {}.o {}.S || exit 255"
 
 echo "linking files"
 OBJ_FILES=$(ls *.o)
+OBJ_FILES+=" "
+OBJ_FILES+=$(ls ../common/*.o)
 echo "Object files to link: $OBJ_FILES"
 riscv64-unknown-elf-ld -T ../link.lds -o $1.elf $OBJ_FILES
 
