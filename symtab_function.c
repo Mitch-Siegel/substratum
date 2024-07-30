c46f50dff931361ff607499988483a8c30ebb297
Mitch Siegel <mitch.siegel.a@gmail.com>
2024-07-29 23:00:57 -0400

    print even prettier cfgs

--- symtab_function.c
+++ symtab_function.c
@@ -44,86 +44,104 @@ void function_entry_free(struct FunctionEntry *function)
     free(function);
 }
 
+void print_graphviz_string(char *str, FILE *outFile)
+{
+    while (*str != '\0')
+    {
+        switch (*str)
+        {
+        case '\n':
+            fprintf(outFile, "\\n");
+            break;
+        case '\t':
+            fprintf(outFile, "\\t");
+            break;
+        case '\"':
+            fprintf(outFile, "\\\"");
+            break;
+        case '\\':
+            fprintf(outFile, "\\\\");
+            break;
+        case '>':
+            fprintf(outFile, "&gt;");
+            break;
+        case '<':
+            fprintf(outFile, "&lt;");
+            break;
+        default:
+            fprintf(outFile, "%c", *str);
+            break;
+        }
+        str++;
+    }
+}
+
 void function_entry_print_cfg(struct FunctionEntry *function, FILE *outFile)
 {
     fprintf(outFile, "digraph %s_cfg {\n", function->name);
+    fprintf(outFile, "graph [splines=ortho];\n");
     fprintf(outFile, "node [shape=record];\n");
     fprintf(outFile, "entry [label=\"entry\"];\n");
 
     fprintf(outFile, "subgraph cluster_%zd {\n", FUNCTION_EXIT_BLOCK_LABEL);
     fprintf(outFile, "\tlabel=\"exit\";\n");
     fprintf(outFile, "\tstyle=\"rounded, filled\";\n");
-    fprintf(outFile, "bb_%zd_entry [label=\"exit\"];\n", FUNCTION_EXIT_BLOCK_LABEL);
+    fprintf(outFile, "bb_%zd_entry [label=\"done\"];\n", FUNCTION_EXIT_BLOCK_LABEL);
     fprintf(outFile, "}\n");
 
     if (function->BasicBlockList->size == 0)
     {
-        fprintf(outFile, "entry -> bb_%zd_0;\n", FUNCTION_EXIT_BLOCK_LABEL);
+        fprintf(outFile, "entry -> bb_%zd:0;\n", FUNCTION_EXIT_BLOCK_LABEL);
         return;
     }
     else
     {
-        fprintf(outFile, "entry -> bb_%zd_0;\n", ((ssize_t)1));
+        fprintf(outFile, "entry -> bb_%zd:0;\n", ((ssize_t)1));
     }
 
     Iterator *blockIter = NULL;
     for (blockIter = list_begin(function->BasicBlockList); iterator_gettable(blockIter); iterator_next(blockIter))
     {
         struct BasicBlock *block = iterator_get(blockIter);
-        if (block->labelNum == FUNCTION_EXIT_BLOCK_LABEL)
-        {
-            continue;
-        }
 
         fprintf(outFile, "subgraph cluster_%zd {\n", block->labelNum);
-        fprintf(outFile, "\tlabel=\"bb_%zd\";\n", block->labelNum);
-        fprintf(outFile, "\tstyle=\"rounded, filled\";\n");
-        fprintf(outFile, "\tbb_%zd_entry [label=\"entry\"];\n", block->labelNum);
+        fprintf(outFile, "penwidth=0;\n");
 
+        fprintf(outFile, "\tbb_%zu [\n", block->labelNum);
+        fprintf(outFile, "\t\tshape=record\n");
+        fprintf(outFile, "\t\tlabel=<\n");
+        fprintf(outFile, "\t\t\t<table border=\"0\" cellborder=\"0\" cellspacing=\"0\">\n");
+        fprintf(outFile, "\t\t\t\t<tr>\n");
+        fprintf(outFile, "\t\t\t\t\t<td port=\"0\">Basic Block %zu</td>\n", block->labelNum);
+        fprintf(outFile, "\t\t\t\t</tr>\n");
         Iterator *tacIter = NULL;
         for (tacIter = list_begin(block->TACList); iterator_gettable(tacIter); iterator_next(tacIter))
         {
             struct TACLine *line = iterator_get(tacIter);
 
             char *sprintedLine = sprint_tac_line(line);
-            fprintf(outFile, "\tbb_%zd_%zd [label=\"%s\"]\n", block->labelNum, line->index, sprintedLine);
+            fprintf(outFile, "\t\t\t\t<tr>\n");
+            fprintf(outFile, "\t\t\t\t\t<td>");
+            print_graphviz_string(sprintedLine, outFile);
+            fprintf(outFile, "</td>\n");
+            fprintf(outFile, "\t\t\t\t\t<td port=\"%zd\">", line->index);
+            fprintf(outFile, "</td>\n");
+            fprintf(outFile, "\t\t\t\t</tr>\n");
             free(sprintedLine);
         }
         iterator_free(tacIter);
+        fprintf(outFile, "\t\t\t</table>\n");
+        fprintf(outFile, "\t\t>\n");
+        fprintf(outFile, "\t];\n");
 
-        fprintf(outFile, "\t{bb_%zd_entry ", block->labelNum);
-        tacIter = list_begin(block->TACList);
-        while (iterator_gettable(tacIter))
-        {
-            struct TACLine *line = iterator_get(tacIter);
-
-            fprintf(outFile, "-> bb_%zd_%zd ", block->labelNum, line->index);
-            iterator_next(tacIter);
-        }
-        iterator_free(tacIter);
-        fprintf(outFile, "[style=invis]}\n");
-
-        // fprintf(outFile, "{rank=same;");
         fprintf(outFile, "\t}\n");
-    }
-    iterator_free(blockIter);
-
-    for (blockIter = list_begin(function->BasicBlockList); iterator_gettable(blockIter); iterator_next(blockIter))
-    {
-        struct BasicBlock *block = iterator_get(blockIter);
-        if (block->labelNum == FUNCTION_EXIT_BLOCK_LABEL)
-        {
-            continue;
-        }
-
-        Iterator *tacIter = NULL;
         for (tacIter = list_begin(block->TACList); iterator_gettable(tacIter); iterator_next(tacIter))
         {
             struct TACLine *line = iterator_get(tacIter);
             if (tac_line_is_jump(line))
             {
                 ssize_t branchTarget = tac_get_jump_target(line);
-                fprintf(outFile, "\tbb_%zd_%zd -> bb_%zd_entry;\n", block->labelNum, line->index, branchTarget);
+                fprintf(outFile, "\tbb_%zd:%zd -> bb_%zd:0 [penwidth=2];\n", block->labelNum, line->index, branchTarget);
             }
         }
         iterator_free(tacIter);
