b9de27325a7ce942eb8291b21c89bc72daf1522d
Mitch Siegel <mitch.siegel.a@gmail.com>
2025-02-12 21:25:03 -0500

    implement lifetime tests

--- rust-rewrite/src/backend/lifetime.rs
+++ rust-rewrite/src/backend/lifetime.rs
@@ -2,7 +2,6 @@ use std::{cmp::Ordering, collections::HashMap, fmt};
 
 use crate::midend::{control_flow::ControlFlow, ir::IROperand, program_point::ProgramPoint};
 
-
 #[derive(Clone)]
 pub struct Lifetime {
     name: String,
@@ -113,7 +112,7 @@ impl LifetimeSet {
                 for read_operand in ir.read_operands() {
                     lifetimes.record_read_at_point(read_operand, ir.program_point());
                 }
-    
+
                 for write_operand in ir.write_operands() {
                     lifetimes.record_write_at_point(write_operand, ir.program_point());
                 }
@@ -162,27 +161,34 @@ impl LifetimeSet {
 
     pub fn print_numerical(&self) {
         for lifetime in self.lifetimes.values() {
-            println!("{:>20}: [{}-{}]", lifetime.name, lifetime.start, lifetime.end);
+            println!(
+                "{:>20}: [{}-{}]",
+                lifetime.name, lifetime.start, lifetime.end
+            );
         }
     }
 
     pub fn print_graphical(&self) {
         let mut largest_indices_per_depth = Vec::<usize>::new();
         for lifetime in self.lifetimes.values() {
-            while lifetime.end.depth >= largest_indices_per_depth.len()
-            {
+            while lifetime.end.depth >= largest_indices_per_depth.len() {
                 largest_indices_per_depth.push(0);
             }
 
             let start_depth = lifetime.start.depth;
             let end_depth = lifetime.end.depth;
-            
-            largest_indices_per_depth[start_depth] = usize::max(largest_indices_per_depth[start_depth], lifetime.start.index);
-            largest_indices_per_depth[end_depth] = usize::max(largest_indices_per_depth[end_depth], lifetime.end.index);
+
+            largest_indices_per_depth[start_depth] =
+                usize::max(largest_indices_per_depth[start_depth], lifetime.start.index);
+            largest_indices_per_depth[end_depth] =
+                usize::max(largest_indices_per_depth[end_depth], lifetime.end.index);
         }
 
         for lifetime in self.lifetimes.values() {
-            print!("{:>20}: [{}-{}]", lifetime.name, lifetime.start, lifetime.end);
+            print!(
+                "{:>20}: [{}-{}]",
+                lifetime.name, lifetime.start, lifetime.end
+            );
             for depth in 0..lifetime.start.depth {
                 for _index in 0..largest_indices_per_depth[depth] {
                     print!(" ");
@@ -194,15 +200,114 @@ impl LifetimeSet {
                     let current_point = ProgramPoint::new(depth, index);
                     if lifetime.live_at(&current_point) {
                         print!("*");
-                    }
-                    else {
+                    } else {
                         print!(" ");
                     }
                 }
             }
             println!();
-
-
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use crate::midend::program_point::ProgramPoint;
+
+    use super::Lifetime;
+
+    #[test]
+    fn test_lifetime_range() {
+        let mut dummy_lifetime = Lifetime::new(String::from("dummy lifetime"));
+
+        dummy_lifetime.record_read(&ProgramPoint::new(1, 1));
+
+        assert!(dummy_lifetime.start() == ProgramPoint::new(1, 1));
+        assert!(dummy_lifetime.start() == dummy_lifetime.end());
+
+        dummy_lifetime.record_write(&ProgramPoint::new(1, 0));
+
+        assert!(dummy_lifetime.start() == ProgramPoint::new(1, 0));
+        assert!(dummy_lifetime.end() == ProgramPoint::new(1, 1));
+    }
+
+    #[test]
+    fn test_lifetime_format() {
+        let mut dummy_lifetime = Lifetime::new(String::from("my_variable"));
+
+        let start_point = ProgramPoint::new(7, 2);
+        let end_point = ProgramPoint::new(9, 4);
+
+        dummy_lifetime.record_write(&start_point);
+        dummy_lifetime.record_write(&end_point);
+
+        assert_eq!(
+            format!("{}", dummy_lifetime),
+            format!("[my_variable: {}-{}]", start_point, end_point)
+        );
+    }
+
+    #[test]
+    fn test_lifetime_partial_ord() {
+        let mut before = Lifetime::new(String::from("before"));
+        let before_2 = before.clone();
+        let mut after = Lifetime::new(String::from("after"));
+
+        assert_eq!(before.partial_cmp(&before), Some(Ordering::Equal));
+        assert_eq!(before.partial_cmp(&before_2), Some(Ordering::Equal));
+        assert!(before != after);
+
+        before.record_read(&ProgramPoint::new(1, 1));
+        after.record_write(&ProgramPoint::new(2, 1));
+        assert_eq!(before.partial_cmp(&before_2), Some(Ordering::Less));
+        assert_eq!(before.partial_cmp(&after), Some(Ordering::Less));
+    }
+
+    #[test]
+    fn test_lifetime_ord() {
+        let mut before = Lifetime::new(String::from("before"));
+        let before_2 = before.clone();
+        let mut after = Lifetime::new(String::from("after"));
+
+        assert_eq!(before.cmp(&before), Ordering::Equal);
+        assert_eq!(before.cmp(&before_2), Ordering::Equal);
+        assert!(before != after);
+
+        before.record_read(&ProgramPoint::new(1, 1));
+        after.record_write(&ProgramPoint::new(2, 1));
+        assert_eq!(before.cmp(&before_2), Ordering::Less);
+        assert_eq!(before.cmp(&after), Ordering::Less);
+    }
+
+    #[test]
+    fn test_lifetime_live_at_within_depth() {
+        let mut dummy_lifetime = Lifetime::new(String::from("dummy lifetime"));
+
+        assert!(!dummy_lifetime.live_at(&ProgramPoint::new(0, 0)));
+        dummy_lifetime.record_write(&ProgramPoint::new(2, 0));
+        dummy_lifetime.record_write(&ProgramPoint::new(2, 5));
+
+        for index in 0..5 {
+            assert!(dummy_lifetime.live_at(&ProgramPoint::new(2, index)));
+        }
+    }
+
+    #[test]
+    fn test_lifetime_live_at_across_depth() {
+        let mut dummy_lifetime = Lifetime::new(String::from("dummy lifetime"));
+
+        assert!(!dummy_lifetime.live_at(&ProgramPoint::new(0, 0)));
+        dummy_lifetime.record_write(&ProgramPoint::new(2, 0));
+        dummy_lifetime.record_write(&ProgramPoint::new(4, 5));
+
+        for depth in 2..=4 {
+            for index in 0..5 {
+                assert!(dummy_lifetime.live_at(&ProgramPoint::new(depth, index)));
+            }
+        }
+
+        assert!(!dummy_lifetime.live_at(&ProgramPoint::new(4, 6)));
     }
 }
