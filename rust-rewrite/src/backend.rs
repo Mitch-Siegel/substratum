674532da7d454ceeffeb6dde5b3f8e244575d3d6
Mitch Siegel <mitch.siegel.a@gmail.com>
2025-05-04 13:16:56 -0400

    rename all mod.rs files

---
+++ rust-rewrite/src/backend.rs
@@ -0,0 +1,17 @@
+mod regalloc;
+
+use crate::midend;
+
+pub fn generate_code_for_function(function: &mut midend::symtab::Function) {
+    println!("generate code for {}", function.prototype);
+    regalloc::allocate_registers(&function.scope, &function.control_flow);
+}
+
+pub fn generate_code(mut symbol_table: midend::symtab::SymbolTable) {
+    for (_, member) in &mut symbol_table.functions {
+        match member {
+            midend::symtab::FunctionOrPrototype::Function(f) => generate_code_for_function(f),
+            midend::symtab::FunctionOrPrototype::Prototype(p) => println!("{}", p),
+        }
+    }
+}
