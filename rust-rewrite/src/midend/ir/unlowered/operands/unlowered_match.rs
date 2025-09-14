034b61f11020a3b5ae9b1b0d8591708b1d89a000
Mitch Siegel <mitch.siegel.a@gmail.com>
2025-09-14 16:20:27 -0400

    start actually linearizing match arm branching logic

--- rust-rewrite/src/midend/ir/unlowered/operands/unlowered_match.rs
+++ rust-rewrite/src/midend/ir/unlowered/operands/unlowered_match.rs
@@ -1,10 +1,39 @@
-use crate::midend::{ir::unlowered::*, *};
+use crate::midend::{ir::unlowered::*, *, linearizer::CustomReturnWalk};
+
+
+struct MatchArmContext<'a> {
+    pub ctx: &'a mut linearizer::FunctionWalkContext,
+    pub scrutinee: ValueId,
+}
+
+struct MatchArmResult<'a> {
+    pub arm_label: usize, // the label of the arm to which we should jump if matched
+    pub result_value: ValueId,
+    pub comparison_value: ValueId,
+    pub ctx: &'a mut linearizer::FunctionWalkContext,
+}
+
 
 #[derive(Debug, Serialize, PartialEq, Eq, Clone)]
 pub struct MatchArm {
     pub pattern: frontend::ast::expressions::match_expression::PatternTree,
     pub arm_label: usize,
     pub result_value: ValueId,
+}
+
+impl<'a> linearizer::CustomReturnWalk<MatchArmContext::<'a>, MatchArmResult::<'a>> for MatchArm {
+    fn walk(self, arm_context: MatchArmContext<'a>) -> MatchArmResult<'a> {
+        let MatchArmContext::<'a> {ctx, scrutinee} = arm_context;
+
+        let comparison_value = ValueId::new(123);
+
+        MatchArmResult {
+            arm_label: self.arm_label,
+            result_value: self.result_value,
+            comparison_value,
+            ctx
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -14,13 +43,12 @@ pub struct MatchOperands {
 }
 
 impl Lowerable for MatchOperands {
-    fn lower(self, context: &mut linearizer::FunctionWalkContext) {
-        context
-            .append_statement_to_current_block(IrLine::new_load(
-                SourceLoc::new(&std::path::Path::new("asdf"), 9999, 9999),
-                ValueId::new(1234),
-                ValueId::new(1235),
-            ))
-            .unwrap();
+    fn lower<'a>(self, mut context: &'a mut linearizer::FunctionWalkContext) {
+        for arm in self.arms {
+            let result = arm.walk(MatchArmContext::<'a> {ctx: context, scrutinee: self.scrutinee});
+
+            context = result.ctx;
+
+        }
     }
 }
